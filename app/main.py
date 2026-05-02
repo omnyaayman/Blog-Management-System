28e8f3124dce4e9b3c46f3df8067ffca3489fcfa
omnyaayman <omnyaayman033gmail.com>
2026-05-02 12:34:54 +0300

    add posts system

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
-
 from fastapi import FastAPI
+
+from app.routes import posts  
+from app.routes import auth    
 
 app = FastAPI()
 
+app.include_router(auth.router)
+app.include_router(posts.router) 
 @app.get("/")
 def root():
     return {"message": "API is running"}
+
+
